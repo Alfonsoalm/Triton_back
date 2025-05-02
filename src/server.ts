31cf060aef8cc1bef9297fe5c73717ea81c1b454
Alfonso Almenara López <alfonsoalm34@gmail.com>
2025-05-02 09:30:46 +0200

    fix(port): puerto cambiado de escucha

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@
 // server.ts
 import { app } from "./app";
 import { connectDB, sequelize } from "./_config/connection";
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 
 (async () => {
   await connectDB(); // Se intenta la conexión a la base de datos
   await sequelize.sync(); // Esto sincroniza los modelos con la BD
   app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://tritonback-production.up.railway.app:${PORT}`);
   });
 })();
