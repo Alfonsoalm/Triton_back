75f3f05989768449cafe71e82683a2bccff25d2f
Alfonso Almenara López <alfonsoalm34@gmail.com>
2025-05-02 10:33:55 +0200

    commit cors

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,10 @@ import { appRouter } from "./_routes/routes";
 
 export const app = express();
 
-app.use(cors({
-  origin: 'https://tritonfront-production.up.railway.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tritonfront-production.up.railway.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+// Middleware
 app.use(express.json());
+
+app.use(cors());
 
 // Rutas principales
 app.use('/api', appRouter); // Las rutas estarán bajo el prefijo '/api'
