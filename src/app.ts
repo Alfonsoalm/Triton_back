820537585064a33591d636834278172efbb4555d
Alfonso Almenara López <alfonsoalm34@gmail.com>
2025-05-02 09:45:20 +0200

    fix(app): cambio cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ export const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: '*', // O usa '*' para permitir desde cualquier dominio (aunque no es recomendado en producción)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Rutas principales
 app.use('/api', appRouter); // Las rutas estarán bajo el prefijo '/api'
